057a0898f2c6a259d3075aa621a5081e913e1340
peter-zoltan <peter.zoltan2004@gmail.com>
2025-03-11 20:13:52 +0100

    Added class declarations to classes.h

--- classes.h
+++ classes.h
@@ -5,4 +5,18 @@
 #ifndef CLASSES_H
 #define CLASSES_H
 
+class Player {
+    bool* memory = nullptr;
+    int memLength = 1;
+public:
+    bool strategy() const;
+};
+
+class GameManager {
+    Player* players = nullptr;
+    int rounds = 1;
+public:
+    void runGame() const;
+};
+
 #endif //CLASSES_H
--- main.cpp
+++ main.cpp
@@ -1,1 +1,8 @@
+#include <iostream>
 
+#include "classes.h"
+
+int main () {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
