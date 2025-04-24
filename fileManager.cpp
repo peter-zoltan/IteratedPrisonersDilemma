74d87c8ab3a545e6e52dfb4783d4aa5b0792b9c8
peter-zoltan <peter.zoltan2004@gmail.com>
2025-04-24 15:07:20 +0200

    Doxygen comments for fileManager.h/cpp

--- fileManager.cpp
+++ fileManager.cpp
@@ -2,8 +2,13 @@
 // Created by peter on 2025. 04. 19..
 //
 
+/**
+ * @file fileManager.h
+ * Source file for the class FileManager.
+ */
+
 #include "fileManager.h"
 
 #include "memtrace.h"
 
-void FileManager::saveToFile(const char* filename, const GameManager& GM) const { }
+void FileManager::saveToFile(const char* filename, const GameManager& gm) const { }
--- fileManager.h
+++ fileManager.h
@@ -2,13 +2,29 @@
 // Created by peter on 2025. 04. 19..
 //
 
+/**
+ * @file fileManager.h
+ * Header file for the class FileManager.
+ */
+
 #ifndef FILEMANAGER_H
 #define FILEMANAGER_H
 #include "gameManager.h"
 
+/**
+ * @class FileManager
+ * Handles saving to file.
+ */
 class FileManager {
+
 public:
-    void saveToFile(const char*, const GameManager& GM) const;
+
+    /**
+     * Writes a game's results (number of rounds, players' name, ID and scores) to file.
+     * @param filename Name of the file to where it writes.
+     * @param gm The instance of GameManager from which data should be saved to file.
+     */
+    void saveToFile(const char* filename, const GameManager& gm) const;
 };
 
 #endif //FILEMANAGER_H
